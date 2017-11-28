e7e2c9c2d5b280354c2d8e52ce202194eba40d9c
Devin Moore <devin.moore@volken.org>
2017-11-28 02:33:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,9 @@ function add2(n) {
   
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+function funkyFunction() {
+  return "FUNKY!"
+
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
